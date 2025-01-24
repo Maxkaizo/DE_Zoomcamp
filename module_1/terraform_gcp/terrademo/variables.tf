e4a8b484443fa58532a8f1568e84e3489e8e5df5
Maxkaizo <jlmartinezol@outlook.com>
2025-01-23 19:14:17 -0600

    modified:   module_1/terraform_gcp/terrademo/main.tf 	modified:   module_1/terraform_gcp/terrademo/variables.tf

--- module_1/terraform_gcp/terrademo/variables.tf
+++ module_1/terraform_gcp/terrademo/variables.tf
@@ -1,29 +1,34 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+}
+
 variable "project" {
   description = "Project"
-  default = "dataeng-448500"
+  default     = "dataeng-448500"
 }
 
 variable "region" {
-    description = "Region"
-    default = "us-central1"
+  description = "Region"
+  default     = "us-central1"
 }
 
 variable "location" {
   description = "Project Location"
-  default = "US"
+  default     = "US"
 }
 
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
-  default = "demo_dataset"
+  default     = "demo_dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
-  default = "dataeng-448500-terra-bucket-demo"
+  default     = "dataeng-448500-terra-bucket-demo"
 }
 
 variable "gcs_storage_class" {
-    description = "Bucket Storage Class"
-    default = "STANDARD"
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
 }
