5dc97c8ec1b0565d14ac0ac81b72825a66e40581
José Luis Martínez Olvera <jlmartinezol@outlook.com>
2025-02-26 18:38:54 +0000

    added fhv staging model

--- module_4/models/staging/stg_fhv_tripdata.sql
+++ module_4/models/staging/stg_fhv_tripdata.sql
@@ -1,7 +1,35 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
+{{ config(materialized="view") }}
 
-select * from {{ source('staging', 'fhv_tripdata') }}
+with tripdata as
+(
+    select *,
+      row_number() over (partition by dispatching_base_num, pickup_datetime, dropoff_datetime) as rn
+    from {{ source("staging", "fhv_tripdata") }}
+    where dispatching_base_num is not null
+)
+select
+    -- identifiers
+    {{
+        dbt_utils.generate_surrogate_key(
+            ["dispatching_base_num", "pickup_datetime", "dropOff_datetime"]
+        )
+    }} as tripid,
+    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }}
+    as pickup_locationid,
+    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }}
+    as dropoff_locationid,
+
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+    -- trip info
+    dispatching_base_num,
+    sr_flag,
+    affiliated_base_number
+
+from tripdata
+where rn = 1
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
